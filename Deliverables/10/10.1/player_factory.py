2e732315b6fe801cc6d1421c5f7e2a10fc64fd2e
Kelly Wachtel <kwachtel24@gmail.com>
2019-12-11 21:48:33 -0600

    GUI working

--- Deliverables/10/10.1/player_factory.py
+++ Deliverables/10/10.1/player_factory.py
@@ -1,12 +1,16 @@
-from player import ProxyPlayer
+from player import ProxyConnectionPlayer
 from player import RandomStrategyPlayer
+from player import ProxyStateContractPlayer, ProxyStateContractStrategyPlayer
+from player import ProxyTypeContractConnectionPlayer, ProxyTypeContractStrategyPlayer
 from player_strategy import create_strategy
 import importlib.util
 from definitions import DEFAULT_PLAYER_CLASS, GUI_PLAYER_CLASS
 import random
 
 
+
 class PlayerFactory():
+    
     def __init__(self, connection=None, path=None, remote=False, gui=False):
         self.connection = connection
         self.path = path
@@ -24,7 +28,7 @@ class PlayerFactory():
             return self.createRemote()
 
     def createProxy(self):
-        player = ProxyPlayer()
+        player = ProxyTypeContractConnectionPlayer(ProxyStateContractPlayer(ProxyConnectionPlayer()))
         player.set_conn(self.connection)
         return player
 
@@ -32,7 +36,13 @@ class PlayerFactory():
         module = __import__(self.path, fromlist=[DEFAULT_PLAYER_CLASS])
         myclass = getattr(module, DEFAULT_PLAYER_CLASS)
         strategy = create_strategy()
-        player = myclass()
+        player = ProxyTypeContractStrategyPlayer(ProxyStateContractStrategyPlayer(myclass()))
+        player.set_strategy(strategy)
+        return player
+    
+    def createRemote(self):
+        strategy = create_strategy()
+        player = ProxyTypeContractStrategyPlayer(ProxyStateContractStrategyPlayer(RandomStrategyPlayer()))
         player.set_strategy(strategy)
         return player
 
@@ -42,8 +52,3 @@ class PlayerFactory():
         player = myclass()
         return player
     
-    def createRemote(self):
-        strategy = create_strategy()
-        player = RandomStrategyPlayer()
-        player.set_strategy(strategy)
-        return player
